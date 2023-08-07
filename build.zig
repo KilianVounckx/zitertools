839faaee17ae778bc438a57fa0f9802d660b2f37
KilianVounckx <kilianvounckx@hotmail.be>
2023-08-07 10:56:26 +0200

    Update build.zig
    Debuggin new build system and package manager

--- build.zig
+++ build.zig
@@ -15,19 +15,9 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "itertools",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+    const module = b.addModule(.{
+        .source_file = .{ .path = "src/main.zig" },
     });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -36,6 +26,11 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    var dep_iter = module.dependencies.iterator();
+    while (dep_iter.next()) |entry| {
+        main_tests.addModule(entry.key_ptr.*, entry.value_ptr.*);
+    }
 
     const run_main_tests = b.addRunArtifact(main_tests);
 
