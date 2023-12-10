cf5b5cbf9510241799ec2866f4da53cb6b0e5f48
Danik Vitek <x3665107@gmail.com>
2023-12-10 20:15:37 +0200

    Make `filterMap`'s `func` comptime. Add `fiterMapContext`

--- src/filter_map_iter.zig
+++ src/filter_map_iter.zig
@@ -9,13 +9,13 @@ const sliceIter = itertools.sliceIter;
 /// An iterator that uses f to both filter and map elements from iter.
 ///
 /// See `map` for more info.
-pub fn FilterMapIter(comptime BaseIter: type, comptime Dest: type) type {
+pub fn FilterMapIter(comptime BaseIter: type, comptime func: anytype) type {
     return struct {
         const Self = @This();
 
         base_iter: BaseIter,
-        func: *const fn (Item(BaseIter)) ?Dest,
 
+        pub const Dest = @typeInfo(@typeInfo(@TypeOf(func)).Fn.return_type.?).Optional.child;
         pub const Next = if (IterError(BaseIter)) |ES| ES!?Dest else ?Dest;
 
         pub fn next(self: *Self) Next {
@@ -26,34 +26,41 @@ pub fn FilterMapIter(comptime BaseIter: type, comptime Dest: type) type {
                 self.base_iter.next();
 
             const item = maybe_item orelse return null;
-            return if (self.func(item)) |transformed|
+            return if (func(item)) |transformed|
                 transformed
-            else if (has_error)
-                try self.next()
             else
-                self.next();
+                @call(.always_tail, Self.next, .{self}); // no need to `try` because the error union, if any, stays the same
         }
     };
 }
 
-/// Returns the destination type for a given base iterator and function type
-pub fn FilterMapDestType(comptime BaseIter: type, comptime Func: type) type {
-    const Source = Item(BaseIter);
+pub fn FilterMapContextIter(
+    comptime BaseIter: type,
+    comptime Context: type,
+    comptime func: anytype,
+) type {
+    return struct {
+        const Self = @This();
 
-    const func = switch (@typeInfo(Func)) {
-        .Fn => |func| func,
-        else => @compileError("filterMap func must be a function"),
-    };
+        base_iter: BaseIter,
+        context: Context,
 
-    if (func.params.len != 1)
-        @compileError("filterMap func must be a unary function");
+        pub const Dest = @typeInfo(@typeInfo(@TypeOf(func)).Fn.return_type.?).Optional.child;
+        pub const Next = if (IterError(BaseIter)) |ES| ES!?Dest else ?Dest;
 
-    if (func.params[0].type.? != Source)
-        @compileError("filterMap func's argument must be iter's item type");
+        pub fn next(self: *Self) Next {
+            const has_error = comptime IterError(BaseIter) != null;
+            const maybe_item = if (has_error)
+                try self.base_iter.next()
+            else
+                self.base_iter.next();
 
-    return switch (@typeInfo(func.return_type.?)) {
-        .Optional => |optional| optional.child,
-        else => @compileError("filterMap func's return type must be an optional"),
+            const item = maybe_item orelse return null;
+            return if (func(self.context, item)) |transformed|
+                transformed
+            else
+                @call(.always_tail, Self.next, .{self}); // no need to `try` because the error union, if any, stays the same
+        }
     };
 }
 
@@ -64,9 +71,36 @@ pub fn FilterMapDestType(comptime BaseIter: type, comptime Func: type) type {
 /// filterMap can be used to make chains of filter and map more concise.
 pub fn filterMap(
     iter: anytype,
-    func: anytype,
-) FilterMapIter(@TypeOf(iter), FilterMapDestType(@TypeOf(iter), @TypeOf(func))) {
-    return .{ .base_iter = iter, .func = func };
+    comptime func: anytype,
+) FilterMapIter(@TypeOf(iter), validateFilterMapFn(Item(@TypeOf(iter)), func)) {
+    return .{ .base_iter = iter };
+}
+
+fn validateFilterMapFn(
+    comptime Source: type,
+    comptime func: anytype,
+) fn (Source) ?@typeInfo(@typeInfo(@TypeOf(func)).Fn.return_type.?).Optional.child {
+    return func;
+}
+
+pub fn filterMapContext(
+    iter: anytype,
+    context: anytype,
+    comptime func: anytype,
+) FilterMapContextIter(
+    @TypeOf(iter),
+    @TypeOf(context),
+    validateFilterMapContextFn(Item(@TypeOf(iter)), @TypeOf(context), func),
+) {
+    return .{ .base_iter = iter, .context = context };
+}
+
+fn validateFilterMapContextFn(
+    comptime Source: type,
+    comptime Context: type,
+    comptime func: anytype,
+) fn (Context, Source) ?@typeInfo(@typeInfo(@TypeOf(func)).Fn.return_type.?).Optional.child {
+    return func;
 }
 
 test "FilterMapIter" {
@@ -89,6 +123,54 @@ test "FilterMapIter" {
     try testing.expectEqual(@as(?u64, 2), iter.next());
     try testing.expectEqual(@as(?u64, 3), iter.next());
     try testing.expectEqual(@as(?u64, 4), iter.next());
+    try testing.expectEqual(@as(?u64, null), iter.next());
+    try testing.expectEqual(@as(?u64, null), iter.next());
+}
+
+test "FilterMapContextIter simple" {
+    const slice: []const u32 = &.{ 1, 2, 3, 4, 5, 6, 7, 8 };
+    var slice_iter = sliceIter(u32, slice);
+
+    const func = struct {
+        pub fn func(context: u32, x: u32) ?u64 {
+            return if (x % context == 0)
+                x / context
+            else
+                null;
+        }
+    }.func;
+
+    var iter = filterMapContext(slice_iter, @as(u32, 2), func);
+
+    try testing.expectEqual(u64, Item(@TypeOf(iter)));
+    try testing.expectEqual(@as(?u64, 1), iter.next());
+    try testing.expectEqual(@as(?u64, 2), iter.next());
+    try testing.expectEqual(@as(?u64, 3), iter.next());
+    try testing.expectEqual(@as(?u64, 4), iter.next());
+    try testing.expectEqual(@as(?u64, null), iter.next());
+    try testing.expectEqual(@as(?u64, null), iter.next());
+}
+
+test "FilterMapContextIter closure" {
+    const slice: []const u32 = &.{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    var slice_iter = sliceIter(u32, slice);
+
+    const Closure = struct {
+        divider: u32,
+        pub fn func(self: @This(), x: u32) ?u64 {
+            return if (x % self.divider == 0)
+                x / self.divider
+            else
+                null;
+        }
+    };
+
+    var iter = filterMapContext(slice_iter, Closure{ .divider = 3 }, Closure.func);
+
+    try testing.expectEqual(u64, Item(@TypeOf(iter)));
+    try testing.expectEqual(@as(?u64, 1), iter.next());
+    try testing.expectEqual(@as(?u64, 2), iter.next());
+    try testing.expectEqual(@as(?u64, 3), iter.next());
     try testing.expectEqual(@as(?u64, null), iter.next());
     try testing.expectEqual(@as(?u64, null), iter.next());
 }
