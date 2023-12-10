7eb0e04567d5d59dd5c8dd5e30bbad159f95b6b0
Danik Vitek <x3665107@gmail.com>
2023-12-10 23:48:43 +0200

    Make `successors`' `func` comptime. Add `successorsContext`

--- src/successors_iter.zig
+++ src/successors_iter.zig
@@ -9,16 +9,30 @@ const range = itertools.range;
 /// An new iterator where each successive item is computed based on the preceding one.
 ///
 /// See `successors` for more info.
-pub fn SuccessorsIter(comptime T: type) type {
+pub fn SuccessorsIter(comptime T: type, comptime func: fn (T) ?T) type {
     return struct {
         const Self = @This();
 
         current: ?T,
-        func: *const fn (T) ?T,
 
         pub fn next(self: *Self) ?T {
             const current = self.current orelse return null;
-            self.current = self.func(current);
+            self.current = func(current);
+            return current;
+        }
+    };
+}
+
+pub fn SuccessorsContextIter(comptime T: type, comptime Context: type, comptime func: fn (Context, T) ?T) type {
+    return struct {
+        const Self = @This();
+
+        current: ?T,
+        context: Context,
+
+        pub fn next(self: *Self) ?T {
+            const current = self.current orelse return null;
+            self.current = func(self.context, current);
             return current;
         }
     };
@@ -27,13 +41,26 @@ pub fn SuccessorsIter(comptime T: type) type {
 /// Creates a new iterator where each successive item is computed based on the
 /// preceding one.
 ///
-/// The iterator starts with the given first item (if any) and calls the given
+/// The iterator starts with the given first item and calls the given
 /// function to compute each item’s successor.
 pub fn successors(
     init: anytype,
-    func: *const fn (@TypeOf(init)) ?@TypeOf(init),
-) SuccessorsIter(@TypeOf(init)) {
-    return .{ .current = init, .func = func };
+    comptime func: fn (@TypeOf(init)) ?@TypeOf(init),
+) SuccessorsIter(@TypeOf(init), func) {
+    return .{ .current = init };
+}
+
+/// Creates a new iterator where each successive item is computed based on the
+/// preceding one, given the context.
+///
+/// The iterator starts with the given first item and calls the given
+/// function to compute each item’s successor.
+pub fn successorsContext(
+    init: anytype,
+    context: anytype,
+    comptime func: fn (@TypeOf(context), @TypeOf(init)) ?@TypeOf(init),
+) SuccessorsContextIter(@TypeOf(init), @TypeOf(context), func) {
+    return .{ .current = init, .context = context };
 }
 
 test "successors" {
@@ -54,4 +81,25 @@ test "successors" {
     try testing.expectEqual(@as(?u32, null), iter.next());
     try testing.expectEqual(@as(?u32, null), iter.next());
     try testing.expectEqual(@as(?u32, null), iter.next());
+}
+
+test "successorsContext fibbonacci" {
+    const func = struct {
+        fn fib(context: *u32, x: u32) ?u32 {
+            defer context.* += x;
+            return context.*;
+        }
+    }.fib;
+    var context: u32 = 1;
+    var iter = successorsContext(@as(u32, 0), &context, func);
+    try testing.expectEqual(u32, Item(@TypeOf(iter)));
+    try testing.expectEqual(@as(?u32, 0), iter.next());
+    try testing.expectEqual(@as(?u32, 1), iter.next());
+    try testing.expectEqual(@as(?u32, 1), iter.next());
+    try testing.expectEqual(@as(?u32, 2), iter.next());
+    try testing.expectEqual(@as(?u32, 3), iter.next());
+    try testing.expectEqual(@as(?u32, 5), iter.next());
+    try testing.expectEqual(@as(?u32, 8), iter.next());
+    try testing.expectEqual(@as(?u32, 13), iter.next());
+    try testing.expectEqual(@as(?u32, 21), iter.next());
 }
