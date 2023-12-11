ab9246548e8495c42e23aa3958880329a6ad0e93
Danik Vitek <x3665107@gmail.com>
2023-12-11 20:48:49 +0200

    TODO: fix compile errors

--- src/zip_iter.zig
+++ src/zip_iter.zig
@@ -1,50 +1,111 @@
 const std = @import("std");
 const testing = std.testing;
+const Type = std.builtin.Type;
+const Struct = std.builtin.Type.Struct;
+const StructField = std.builtin.Type.StructField;
+const ErrorSet = std.builtin.Type.ErrorSet;
+const Error = std.builtin.Type.Error;
 
 const itertools = @import("main.zig");
 const Item = itertools.Item;
+const SliceIter = itertools.SliceIter;
 const IterError = itertools.IterError;
 const sliceIter = itertools.sliceIter;
 const range = itertools.range;
+const iterMethods = itertools.iterMethods;
 
 /// An iterator that iterates two other iterators simultaneously.
 ///
 /// See `zip` for more info.
-pub fn ZipIter(comptime A: type, comptime B: type) type {
+pub fn ZipIter(comptime Iters: type) type {
     return struct {
         const Self = @This();
 
-        a: A,
-        b: B,
+        iters: Iters,
 
-        pub const Item = struct { itertools.Item(A), itertools.Item(B) };
-
-        pub const Next = if (IterError(A)) |ESA|
-            if (IterError(B)) |ESB|
-                (ESA || ESB)!?Self.Item
-            else
-                ESA!?Self.Item
-        else if (IterError(B)) |ESB|
-            ESB!?Self.Item
-        else
-            ?Self.Item;
+        pub const Item: type = ZipIterItem(Iters);
+        pub const ErrorSet: ?type = ZipIterErrorSet(Iters);
+        pub const Next = if (Self.ErrorSet) |ES| ES!?Self.Item else ?Self.Item;
 
         pub fn next(self: *Self) Next {
-            const a_has_error = comptime IterError(A) != null;
-            const b_has_error = comptime IterError(B) != null;
-            const maybe_a = if (a_has_error)
-                try self.a.next()
-            else
-                self.a.next();
-            const a = maybe_a orelse return null;
-            const maybe_b = if (b_has_error)
-                try self.b.next()
-            else
-                self.b.next();
-            const b = maybe_b orelse return null;
-            return .{ a, b };
+            var item: Self.Item = undefined;
+            inline for (@typeInfo(Iters).Struct.fields) |iter_field| {
+                if (iter_field.is_comptime) {
+                    continue;
+                }
+
+                const has_error = comptime IterError(iter_field.type) != null;
+                const maybe_item = if (has_error)
+                    try @field(self.iters, iter_field.name).next()
+                else
+                    @field(self.iters, iter_field.name).next();
+                @field(item, iter_field.name) = maybe_item orelse return null;
+            }
+            return item;
         }
     };
+}
+
+pub fn ZipIterItem(comptime Iters: type) type {
+    const iters_type_info = @typeInfo(Iters);
+    const total_fields = blk: {
+        var total_fields = 0;
+        for (iters_type_info.Struct.fields) |field| {
+            if (field.is_comptime) {
+                continue;
+            }
+            total_fields += 1;
+        }
+        break :blk total_fields;
+    };
+    if (iters_type_info != .Struct) {
+        @compileError("expected tuple or struct, found '" ++ @typeName(Iters) ++ "'");
+    }
+    return @Type(.{
+        .Struct = Struct{
+            .layout = .Auto,
+            .fields = &blk: {
+                var fields: [total_fields]StructField = undefined;
+                var i = 0; // manual counter because of filtering out of comptime fields
+                for (iters_type_info.Struct.fields) |field| {
+                    if (field.is_comptime) {
+                        continue;
+                    }
+                    fields[i] = std.builtin.Type.StructField{
+                        .name = field.name,
+                        .type = Item(field.type),
+                        .default_value = null,
+                        .is_comptime = false,
+                        .alignment = 0,
+                    };
+                    i += 1;
+                }
+                break :blk fields;
+            },
+            .decls = &.{},
+            .is_tuple = iters_type_info.Struct.is_tuple,
+        },
+    });
+}
+
+pub fn ZipIterErrorSet(comptime Iters: type) ?type {
+    const iters_type_info = @typeInfo(Iters);
+    if (iters_type_info != .Struct) {
+        @compileError("expected tuple or struct, found '" ++ @typeName(Iters) ++ "'");
+    }
+
+    return comptime iterMethods(sliceIter(StructField, iters_type_info.Struct.fields)).filterMap(struct {
+        fn isErrorUnion(field: StructField) ?type {
+            return switch (@typeInfo(field.type)) {
+                .ErrorUnion => |ErrorUnion| ErrorUnion.error_set,
+                else => null,
+            };
+        }
+    }.isErrorUnion).reduce(struct {
+        fn ErrorUnion(comptime Error1: type, comptime Error2: type) type {
+            return Error1 || Error2;
+        }
+    }.ErrorUnion);
 }
 
 /// ‘Zips up’ two iterators into a single iterator of pairs.
@@ -60,14 +121,14 @@ pub fn ZipIter(comptime A: type, comptime B: type) type {
 /// further attempt to advance it will first try to advance the first iterator
 /// at most one time and if it still yielded an item try to advance the second
 /// iterator at most one time.
-pub fn zip(iter1: anytype, iter2: anytype) ZipIter(@TypeOf(iter1), @TypeOf(iter2)) {
-    return .{ .a = iter1, .b = iter2 };
+pub fn zip(iters: anytype) ZipIter(@TypeOf(iters)) {
+    return .{ .iters = iters };
 }
 
 test "zip" {
     var iter1 = sliceIter(u32, &.{ 1, 2, 3 });
     var iter2 = range(u64, 5, 8);
-    var iter = zip(iter1, iter2);
+    var iter = zip(.{ iter1, iter2 });
     try testing.expectEqual(@TypeOf(iter).Item, Item(@TypeOf(iter)));
     const v1 = iter.next().?;
     try testing.expectEqual(@as(u32, 1), v1.@"0");
